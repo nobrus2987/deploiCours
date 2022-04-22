35392470296b1384388c7d1d25230a266701c88e
Lawdut06 <104195727+Lawdut06@users.noreply.github.com>
2022-04-22 15:47:12 +0200

    Delete controllers directory

--- controllers/connectDb.js
+++
@@ -1,9 +0,0 @@
-const mongoClient = require('mongodb').MongoClient;
-
-const urlDb = 'mongodb://localhost:27017';
-const dbname = 'apogee';
-mongoClient.connect(urlDb, {useNewUrlParser: true, useUnifiedTopology: true}, function (error,client){
-    if (error) throw error;
-    exports.db = client.db(dbname);
-    console.log('connexion à la db réussie');
-  })
