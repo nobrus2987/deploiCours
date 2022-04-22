eba406d1a9ed7393966aa47751d2139c44066d46
bruno2987 <bruno.dubief@hotmail.fr>
2022-04-22 16:03:55 +0200

    addition

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 console.log('test')
+
+var test1 = 3
+var test2 = 4
+
+
+function testAddition(x,y) {
+    let z = x + y ;
+    return z
+}
+
+var aFaire = testAddition(test1,test2)
+
+console.log(aFaire)
